33f50ac366281a1d5fa9c6d41508c20de9632515
ondolist <ondolist@naver.com>
2024-11-01 06:16:22 +0000

    send traffic light change request

--- packet.py
+++ packet.py
@@ -1,7 +1,7 @@
 import traci
-import packet
+import utils
 
 
-# Function to set a traffic light to the specified green phase
-def set_traffic_light_to_green(tls_id, green_phase_index):
-    traci.trafficlight.setPhase(tls_id, green_phase_index)
+# Function to send a traffic light change request
+def send_traffic_light_change_request(tls_id, green_phase_index):
+    utils.set_traffic_light_to_green(tls_id, green_phase_index)
